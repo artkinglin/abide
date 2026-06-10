edb40ccc6d2d75fe8c2af360dceef6d80a3dab43
Arthur Lin <artkinglin@gmail.com>
2026-06-10 00:59:18 -0500

    Bound upstream requests with a timeout

--- server.js
+++ server.js
@@ -35,4 +35,15 @@ function requireApiKey(name) {
   return key;
 }
 
+async function fetchWithTimeout(url, options, timeoutMs = 20000) {
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), timeoutMs);
+
+  try {
+    return await fetch(url, { ...options, signal: controller.signal });
+  } finally {
+    clearTimeout(timeout);
+  }
+}
+
 module.exports = app;
